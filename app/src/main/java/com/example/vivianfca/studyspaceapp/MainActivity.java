04afbc460dd350db9d2521f9c7b557882ce8395b
Yingan Wang <yinganw2@illinois.edu>
2018-12-01 18:46:03 -0600

    add filterSearch

--- app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vivianfca.studyspaceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Button search = (Button)
 }
