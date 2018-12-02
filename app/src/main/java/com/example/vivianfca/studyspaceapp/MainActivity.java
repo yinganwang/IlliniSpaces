4baeaf95c7a4f935387fec842085055dce24eb13
Medha Patil <medhap3@illinois.edu>
2018-12-01 19:02:25 -0600

    main activity direct to search done

--- app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.vivianfca.studyspaceapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //direct to google maps button
+        Button directToGMaps = findViewById(R.id.search);
+        directToGMaps.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri navigationIntentUri = Uri.parse("google.navigation:q=" + 12f + "," + 2f);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, navigationIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 }
