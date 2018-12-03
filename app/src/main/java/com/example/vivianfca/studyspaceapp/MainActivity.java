d69e7514047d798ed1bf7b3f7690ac98fbbb182a
Medha Patil <medhap3@illinois.edu>
2018-12-02 20:27:01 -0600

    main activity direct to search done

--- app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.vivianfca.studyspaceapp;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //direct to google maps button.
+        Button directToGMaps = findViewById(R.id.search);
+        directToGMaps.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri navigationIntentUri = Uri.parse("google.navigation:q=" + 12f + "," + 2f);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, navigationIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+                //message to display if maps isn't installed.
+                try
+                {
+                    startActivity(mapIntent);
+                }
+                catch(ActivityNotFoundException ex)
+                {
+                    Toast.makeText(getApplicationContext(), "Please install a maps application", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         //Activity to launch filter activity
         Button filter = (Button) findViewById(R.id.filter);
