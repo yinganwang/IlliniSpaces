cf655f36b128a9bfde63dd6b77dc97fed6203862
Yingan Wang <yinganw2@illinois.edu>
2018-12-02 20:38:45 -0600

    filter activity function development

--- app/src/main/java/com/example/vivianfca/studyspaceapp/FilterActivity.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/FilterActivity.java
@@ -1,6 +1,5 @@
 package com.example.vivianfca.studyspaceapp;
 
-import android.app.VoiceInteractor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
