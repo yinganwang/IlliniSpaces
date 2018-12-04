23b1549a1022e7216d058b3c6d94bd330c94f3e6
Yingan Wang <yinganw2@illinois.edu>
2018-12-04 00:16:43 -0600

    Merge branch 'master' of https://github.com/yinganwang/IlliniSpaces
    # Conflicts:
    #	app/src/main/java/com/example/vivianfca/studyspaceapp/MapActivity.java

--- app/src/main/java/com/example/vivianfca/studyspaceapp/FetchDataforFilter.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/FetchDataforFilter.java
@@ -1,69 +1,69 @@
-//package com.example.vivianfca.studyspaceapp;
-//
-//import android.os.AsyncTask;
-//
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
-//import java.net.URL;
-//
-//public class FetchDataforFilter extends AsyncTask<Void,Void,Void> {
-//    String data = "";
-//    String dataParsed = "";
-//    String singleParsed = "";
-//
-//    @Override
-//    protected Void doInBackground(Void... voids) {
-//        try {
-//            URL url = new URL("https://api.myjson.com/bins/1dwygq");
-//
-//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//
-//            InputStream inputStream = httpURLConnection.getInputStream();
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//            String line = "";
-//            while (line != null) {
-//                line = bufferedReader.readLine();
-//                data = data + line;
-//            }
-//
-//            JSONArray JA = new JSONArray(data);
-//            for (int i = 0; i < JA.length(); i++) {
-//                JSONObject JO = (JSONObject) JA.get(i);
-//                singleParsed = "Name" + JO.get("Name") + "\n"
-//                        + "Building" + JO.get("Building") + "\n"
-//                        + "Hours" + JO.get("Hours") + "\n"
-//                        + "Type of Space" + JO.get("Type of Space") + "\n"
-//                        + "Location" + JO.get("Location") + "\n"
-//                        + "Address" + JO.get("Address") + "\n"
-//                        + "Resources" + JO.get("Resources") + "\n"
-//                        + "Noise Level" + JO.get("Noise Level");
-//                dataParsed = dataParsed + singleParsed + "\n";
-//            }
-//
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    @Override
-//    protected void onPostExecute(Void aVoid) {
-//        super.onPostExecute(aVoid);
-//
-//        //DisplayActivity.data.setText(this.dataParsed);
-//
-//    }
-//
-//}
+package com.example.vivianfca.studyspaceapp;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FetchDataforFilter extends AsyncTask<Void,Void,Void> {
+    String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://api.myjson.com/bins/1dwygq");
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+            for (int i = 0; i < JA.length(); i++) {
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed = "Name" + JO.get("Name") + "\n"
+                        + "Building" + JO.get("Building") + "\n"
+                        + "Hours" + JO.get("Hours") + "\n"
+                        + "Type of Space" + JO.get("Type of Space") + "\n"
+                        + "Location" + JO.get("Location") + "\n"
+                        + "Address" + JO.get("Address") + "\n"
+                        + "Resources" + JO.get("Resources") + "\n"
+                        + "Noise Level" + JO.get("Noise Level");
+                dataParsed = dataParsed + singleParsed + "\n";
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        DisplayActivity.data.setText(this.dataParsed);
+
+    }
+
+}
