a1110cb3df3ce044b0c19c3f783b3ad485f60347
Medha Patil <medhap3@illinois.edu>
2018-12-08 00:49:37 -0600

    map

--- app/src/main/java/com/example/vivianfca/studyspaceapp/MapActivity.java
+++ app/src/main/java/com/example/vivianfca/studyspaceapp/MapActivity.java
@@ -1,21 +1,36 @@
 package com.example.vivianfca.studyspaceapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
 
     private GoogleMap mMap;
     List<Double> lat = new ArrayList<Double>();
@@ -29,6 +44,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
 
 
         // filtered addresses from filter Activity
@@ -50,11 +66,13 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
+
+        // Add markers and move camera.
         for (int i = 0; i < lat.size(); i++) {
 
             LatLng loc = new LatLng(lat.get(i), lng.get(i));
@@ -64,4 +82,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         }
 
     }
+
+    //<-------------------------------------------new current location code------------------------------------------------------>
+
 }
